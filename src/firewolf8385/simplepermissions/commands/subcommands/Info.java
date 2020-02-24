00056ceb163573b74ada70b8658a396456b3fa20
Colby <colbykrawczyk10@gmail.com>
2020-02-23 21:25:39 -0500

    Update Info.java
    Added support for themes.

--- src/firewolf8385/simplepermissions/commands/subcommands/Info.java
+++ src/firewolf8385/simplepermissions/commands/subcommands/Info.java
@@ -1,5 +1,6 @@
 package firewolf8385.simplepermissions.commands.subcommands;
 
+import firewolf8385.simplepermissions.SettingsManager;
 import firewolf8385.simplepermissions.SimplePermissions;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,16 +11,22 @@ import firewolf8385.simplepermissions.utils.*;
 
 public class Info implements CommandExecutor
 {
+    private SettingsManager settings = SettingsManager.getInstance();
+    private String gray = settings.getConfig().getString("Theme.color1");
+    private String dgreen = settings.getConfig().getString("Theme.color2");
+    private String lgreen = settings.getConfig().getString("Theme.color3");
+    private String white = settings.getConfig().getString("Theme.color4");
+
     private static Plugin pl = SimplePermissions.getPlugin();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
-        ChatUtils.chat(sender, "&2&l]&8&m---------------------------------------------------&2&l[");
-        ChatUtils.centeredChat((Player) sender, "&2&lSimplePermissions");
-        ChatUtils.chat(sender, "  &8» &aVersion &8- &f" + pl.getDescription().getVersion());
-        ChatUtils.chat(sender, "  &8» &aAuthor &8- &f" + pl.getDescription().getAuthors().get(0));
-        ChatUtils.chat(sender, "&2&l]&8&m---------------------------------------------------&2&l[");
+        ChatUtils.chat(sender, dgreen + "&l]" + gray + "&m---------------------------------------------------" + dgreen + "&l[");
+        ChatUtils.centeredChat((Player) sender, dgreen + "&lSimplePermissions");
+        ChatUtils.chat(sender, "  " + gray + "» " + lgreen + "Version " + gray + "- " + white + pl.getDescription().getVersion());
+        ChatUtils.chat(sender, "  " + gray + "» " + lgreen + "Author " + gray + "- " + white + pl.getDescription().getAuthors().get(0));
+        ChatUtils.chat(sender, dgreen + "&l]" + gray + "&m---------------------------------------------------" + dgreen + "&l[");
         return true;
     }
 
