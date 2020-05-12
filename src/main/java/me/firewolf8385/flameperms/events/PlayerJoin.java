e3d0eabb83da88a6b7770ad37960a84f7100aefa
Colby <colbykrawczyk10@gmail.com>
2020-05-12 03:33:18 -0400

    Made join event async

--- src/main/java/me/firewolf8385/flameperms/events/PlayerJoin.java
+++ src/main/java/me/firewolf8385/flameperms/events/PlayerJoin.java
@@ -1,7 +1,9 @@
 package me.firewolf8385.flameperms.events;
 
+import me.firewolf8385.flameperms.FlamePerms;
 import me.firewolf8385.flameperms.MySQL;
 import me.firewolf8385.flameperms.api.PlayerAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,24 +17,28 @@ public class PlayerJoin implements Listener
     @EventHandler
     public void onJoin(PlayerJoinEvent e)
     {
-        MySQL.ensureConnection();
 
-        if(!PlayerAPI.playerExists(e.getPlayer()))
-        {
-            try
+        // Run join tasks async to avoid blocking the main thread.
+        Bukkit.getScheduler().runTaskAsynchronously(FlamePerms.getPlugin(), () -> {
+            MySQL.ensureConnection();
+
+            if(!PlayerAPI.playerExists(e.getPlayer()))
             {
-                PreparedStatement statement = MySQL.getConnection().prepareStatement("INSERT INTO fp_users (uuid) VALUES (?)");
-                statement.setString(1, e.getPlayer().getUniqueId().toString());
+                try
+                {
+                    PreparedStatement statement = MySQL.getConnection().prepareStatement("INSERT INTO fp_users (uuid) VALUES (?)");
+                    statement.setString(1, e.getPlayer().getUniqueId().toString());
 
-                statement.executeUpdate();
+                    statement.executeUpdate();
+                }
+                catch(SQLException ex)
+                {
+                    ex.printStackTrace();
+                }
             }
-            catch(SQLException ex)
-            {
-                ex.printStackTrace();
-            }
-        }
 
-        PlayerAPI.assignPermissions(e.getPlayer());
+            PlayerAPI.assignPermissions(e.getPlayer());
+        });
     }
 
 }
